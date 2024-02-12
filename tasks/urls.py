8e70ceedc7166bae6f5eafd24b98cafde5f40318
Gabriel Salvador <gabriel.l.salvador@student.ateneo.edu>
2024-02-12 15:16:33 +0800

    task list templates

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, task_list
 
 urlpatterns = [
     path('', index, name = 'index'),
+    path('list', task_list, name = 'list')
 ]
 
 app_name = 'tasks'
--- tasks/views.py
+++ tasks/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('Hello World')
 
+def task_list(request):
+    ctx = {
+        'tasks': [
+            'Task 1',
+            'Task 2',
+            'Task 3',
+            'Task 4',
+        ],
+    }
+    return render(request, 'task_list.html', ctx)
 # Create your views here.
